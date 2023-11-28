a56396f97750062492310665aab8bdf5943b8c6c
Diego <diegoulloao@icloud.com>
2023-11-28 02:44:22 -0300

    feat: three.js

---
+++ src/components/Scene.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import scene from "@3d/scene.ts";
+
+  let elem: HTMLDivElement;
+
+  onMount(() => scene(elem));
+</script>
+
+<div bind:this={elem} />
